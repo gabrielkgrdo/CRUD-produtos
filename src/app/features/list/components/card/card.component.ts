ed3e6b73f6919f42d6bdcdadec652017fcdc7efb
gabrielkgrdo <gabrielderoque@gmail.com>
2024-06-25 10:55:25 -0300

    refactor: refatora listagem de produto criando um componente para renderizar o mesmo

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '../../../../shared/interfaces/products.interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+  product = input.required<Product>()
+}
