fa475e36849526d47164827844baa18840c37370
gabrielkgrdo <gabrielderoque@gmail.com>
2024-06-25 13:55:28 -0300

    refactor: carrega CreateComponent via lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,8 @@ export const routes: Routes = [{
 },
 {
   path: 'create-product',
-  component: CreateComponent
+  loadComponent: () => import('./features/create/create.component').then(
+    (m) => m.CreateComponent
+  )
 
 }];
