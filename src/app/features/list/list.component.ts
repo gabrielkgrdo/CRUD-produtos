51e9e18175c3017f7162aea76881f6f782257826
gabrielkgrdo <gabrielderoque@gmail.com>
2024-06-26 16:03:44 -0300

    refactor: adiciona defer block na listagem de produtos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -6,21 +6,23 @@ import { Product } from '../../shared/interfaces/products.interface';
 import { ConfirmDialogService } from '../../shared/services/confirm-dialog.service';
 import { ProductsService } from '../../shared/services/products.service';
 import { CardComponent } from './components/card/card.component';
+import { NoItemsComponent } from './components/no-items/no-items.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItemsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
 export class ListComponent {
-  products = signal<Product[]>(inject(ActivatedRoute).snapshot.data['products']);
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+  );
   router = inject(Router);
   confirmDialogService = inject(ConfirmDialogService);
 
   productsService = inject(ProductsService);
-
 
   OnEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
@@ -33,7 +35,7 @@ export class ListComponent {
       .subscribe(() => {
         this.productsService.delete(product.id).subscribe(() => {
           this.productsService.getAll().subscribe((products) => {
-            this.products.set(products)
+            this.products.set(products);
           });
         });
       });
